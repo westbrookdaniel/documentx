da23acc462ab50b5ca1dd401aafe88117eb5394e
westy12dan <westy12dan@gmail.com>
2023-04-29 00:08:38 +0800

    Fix format

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
+    content: ['./index.html', './src/**/*.{ts,tsx}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+    ],
 }
