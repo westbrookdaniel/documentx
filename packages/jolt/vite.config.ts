da396c50c229b802d79ecae2fd60fa3f84df8527
westy12dan <westy12dan@gmail.com>
2023-04-28 17:10:29 +0800

    Add vite building

---
+++ packages/jolt/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: ['./index.ts', './jsx-runtime.ts', './jsx-dev-runtime.ts'],
+      name: 'Jolt',
+    },
+  },
+  plugins: [
+    dts({
+      // Dom has extra types, so use that config
+      tsConfigFilePath: './tsconfig.dom.json',
+    }),
+  ],
+})
