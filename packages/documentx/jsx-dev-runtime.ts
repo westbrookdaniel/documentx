09f8779668f70caf314b945cb8d56a95bc0b2ce4
westy12dan <westy12dan@gmail.com>
2023-05-01 18:47:49 +0800

    fix up some types

--- packages/documentx/jsx-dev-runtime.ts
+++
@@ -1,5 +0,0 @@
-import { Fragment, jsx } from './jsx-runtime'
-
-// It's all the same so we don't care where it comes from
-export { jsx, jsx as jsxs, Fragment }
-export { jsx as jsxDEV, jsx as jsxsDEV }
